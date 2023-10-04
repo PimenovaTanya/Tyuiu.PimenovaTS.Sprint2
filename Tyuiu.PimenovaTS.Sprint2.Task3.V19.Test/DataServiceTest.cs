4d2cf0135a18ff6bcde206b7f703b99eaf6b721a
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-10-04 12:23:02 +0500

    Task3

---
+++ Tyuiu.PimenovaTS.Sprint2.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PimenovaTS.Sprint2.Task3.V19.Lib;
+
+namespace Tyuiu.PimenovaTS.Sprint2.Task3.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 13;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCalculate2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 1.556;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCalculate3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double res = ds.Calculate(x);
+            double wait = 0.64;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCalculate4()
+        {
+            DataService ds = new DataService();
+            double x = -25;
+            double res = ds.Calculate(x);
+            double wait = -274.96;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PimenovaTS.Sprint2.Task3.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PimenovaTS.Sprint2.Task3.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PimenovaTS.Sprint2.Task3.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85285144-198b-4907-823b-6f64c9aa95bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
