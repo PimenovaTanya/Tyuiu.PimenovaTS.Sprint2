7898decab3832d02d9c5b38fd0d6e42a2e9482fb
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-10-09 21:36:42 +0500

    Task6

---
+++ Tyuiu.PimenovaTS.Sprint2.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PimenovaTS.Sprint2.Task6.V12.Lib;
+
+namespace Tyuiu.PimenovaTS.Sprint2.Task6.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay()
+        {
+            DataService ds = new DataService();
+
+            int g = 23;
+            int m = 9;
+            int n = 3;
+
+            double res = ds.FindDateOfPreviousDay(g, m, n);
+            string w = "2392";
+
+
+            Assert.AreEqual(Convert.ToDouble(w), res);
+        }
+    }
+}
---
+++ Tyuiu.PimenovaTS.Sprint2.Task6.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PimenovaTS.Sprint2.Task6.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PimenovaTS.Sprint2.Task6.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1a4f745-f19b-4227-af4d-fbb23b4f94db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
